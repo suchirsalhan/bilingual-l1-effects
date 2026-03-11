68182e85f70197f25bf362c8bcb621e2253ea1a1
lgb35 <lgbarbenel@gmail.com>
2026-03-11 14:24:28 +0000

    Update pl-en-alpacaL1.py

--- bilingual/instruction/instruction tuning scripts (alpaca)/pl-en-alpacaL1.py
+++ bilingual/instruction/instruction tuning scripts (alpaca)/pl-en-alpacaL1.py
@@ -20,10 +20,10 @@ if HF_TOKEN is None:
 # -----------------------------
 # CONFIG
 # -----------------------------
-MODEL_NAME = "RA-ALTA/pl-en-beginner"
+MODEL_NAME = "RA-ALTA/pl-en-intermediate"
 MAX_LENGTH = 512
 IGNORE_INDEX = -100
-HF_REPO = "RA-ALTA/pl-en-beginner-alpaca-english-L1"
+HF_REPO = "RA-ALTA/pl-en-intermediate-alpaca-english-L1"
 LOCAL_DIR = "./dummy_output"  # local save directory
 
 # -----------------------------
