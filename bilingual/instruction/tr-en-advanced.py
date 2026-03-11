202e6e4c1005e2b708b68b97f5f49c1974cd97ec
lgb35 <lgbarbenel@gmail.com>
2026-03-11 01:38:30 +0000

    Update tr-en-advanced.py

--- bilingual/instruction/tr-en-advanced.py
+++ bilingual/instruction/tr-en-advanced.py
@@ -112,8 +112,8 @@ collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True)
 # -----------------------------
 training_args = TrainingArguments(
     output_dir=LOCAL_DIR,
-    per_device_train_batch_size=8,
-    gradient_accumulation_steps=2,
+    per_device_train_batch_size=16,
+    gradient_accumulation_steps=1,
     num_train_epochs=1,
     learning_rate=5e-5,
     bf16=True,
