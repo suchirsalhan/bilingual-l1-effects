5551d5ea06fb57b2a5c0f0b4e41a778d1a9aeddd
Laura Barbenel <lgbarbenel@gmail.com>
2026-05-11 16:49:42 +0100

    Update Instruction_Tuning_Alpaca_English_Polish.py

--- instruction tuning/instruction tuning scripts (alpaca)/Instruction_Tuning_Alpaca_English_Polish.py
+++ instruction tuning/instruction tuning scripts (alpaca)/Instruction_Tuning_Alpaca_English_Polish.py
@@ -10,14 +10,6 @@ from transformers import (
 )
 
 # -----------------------------
-# ENVIRONMENT (HF token + cache)
-# -----------------------------
-os.environ["HF_HOME"] = "/local/scratch/lgb35/hf_cache"
-HF_TOKEN = os.environ.get("HF_TOKEN")  # make sure you exported this in SSH
-if HF_TOKEN is None:
-    raise ValueError("HF_TOKEN environment variable not set! Run: export HF_TOKEN='your_token_here'")
-
-# -----------------------------
 # CONFIG
 # -----------------------------
 MODEL_NAME = "RA-ALTA/pl-en-advanced"
